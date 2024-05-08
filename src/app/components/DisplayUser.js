6d9392011678047d714c22dc9135fbe6ebeb4f6f
pankaj godhani <pankajgodhani99@gmail.com>
2024-05-09 00:42:38 +0530

    add & get userdara for rtk

--- src/app/components/DisplayUser.js
+++ src/app/components/DisplayUser.js
@@ -2,11 +2,14 @@
 import { useSelector } from "react-redux";
 
 const DisplayUser = () => {
-  let userData = useSelector((data) => data.users);
+  let userData = useSelector((data) => data.default.users);
   console.log(userData);
   return (
     <div className="display-user">
       <h3>Display User List</h3>
+      {userData.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
     </div>
   );
 };
